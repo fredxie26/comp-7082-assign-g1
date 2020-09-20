7534b92aa1261a240153c056919d62032f5919ce
JoCwik <jocwik@gmail.com>
2020-09-20 15:30:06 -0700

    SearchActivity: Java class + layout file + tests

---
+++ app/src/main/java/com/bcit/comp7082/group1/SearchActivity.java
@@ -0,0 +1,53 @@
+package com.bcit.comp7082.group1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+import java.util.logging.Logger;
+
+public class SearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+        try {
+            Calendar calendar = Calendar.getInstance();
+            DateFormat format = new SimpleDateFormat("yyy-MM-dd");
+            Date now = calendar.getTime();
+            String todayStr = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(calendar.getTime());
+            Date today = format.parse(todayStr);
+            calendar.add(Calendar.DAY_OF_YEAR, 1);
+            String tomorrowStr = new SimpleDateFormat("yyy-MM-dd", Locale.getDefault()).format(calendar.getTime());
+            Date tomorrow = format.parse(tomorrowStr);
+            ((EditText) findViewById(R.id.etFromDateTime)).setText(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault()).format(today));
+            ((EditText) findViewById(R.id.etToDateTime)).setText(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault()).format(tomorrow));
+        } catch (Exception e) {
+        }
+    }
+
+    public void cancel(final View v) {
+        finish();
+    }
+
+    public void go(final View v) {
+        Intent i = new Intent();
+        EditText from = (EditText) findViewById(R.id.etFromDateTime);
+        EditText to = (EditText) findViewById(R.id.etToDateTime);
+        EditText keywords = (EditText) findViewById(R.id.etKeywords);
+        i.putExtra("STARTTIMESTAMP", from.getText() != null ? from.getText().toString() : "");
+        i.putExtra("ENDTIMESTAMP", to.getText() != null ? to.getText().toString() : "");
+        i.putExtra("KEYWORDS", keywords.getText() != null ? keywords.getText().toString() : "");
+        setResult(RESULT_OK, i);
+        finish();
+    }
+}
