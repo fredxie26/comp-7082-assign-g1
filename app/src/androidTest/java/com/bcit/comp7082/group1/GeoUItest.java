ac1abee3ed551341dc4517cefbccc64933366cbe
RalphNom <53502331+RalphNom@users.noreply.github.com>
2020-10-04 14:14:47 -0700

    Adjust UI for API 30, Prev, Snap, and Next button are now alligned.

---
+++ app/src/androidTest/java/com/bcit/comp7082/group1/GeoUItest.java
@@ -0,0 +1,58 @@
+package com.bcit.comp7082.group1;
+
+import android.location.Location;
+import android.location.LocationManager;
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.clearText;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withResourceName;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+
+public class GeoUItest {
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> activityRule = new ActivityScenarioRule<>(MainActivity.class);
+
+    @Test
+    public void GeoTest() throws IOException
+    {
+
+        String mocLocationProvider = LocationManager.GPS_PROVIDER;
+        Location mockLocation = new Location(mocLocationProvider);
+
+
+        onView(withId(R.id.snap_button)).perform();
+
+        //Searches for GPS
+        onView(withId(R.id.Search)).perform(click());
+        onView(withId(R.id.etLatitude)).perform(typeText("49"), closeSoftKeyboard());
+        onView(withId(R.id.etLongitude)).perform(typeText("-122"), closeSoftKeyboard());
+        onView(withId(R.id.go)).perform(click());
+
+    }
+
+
+}
