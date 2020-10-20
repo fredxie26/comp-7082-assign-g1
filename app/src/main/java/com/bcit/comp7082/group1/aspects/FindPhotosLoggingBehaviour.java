f2e283123175c6102396faeba441c529f1282994
JoCwik <jocwik@gmail.com>
2020-10-19 22:03:58 -0700

    [C7AG-18] S3 Task 4 - Aspect Oriented Programming Feature

---
+++ app/src/main/java/com/bcit/comp7082/group1/aspects/FindPhotosLoggingBehaviour.java
@@ -0,0 +1,11 @@
+package com.bcit.comp7082.group1.aspects;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface FindPhotosLoggingBehaviour {
+}
