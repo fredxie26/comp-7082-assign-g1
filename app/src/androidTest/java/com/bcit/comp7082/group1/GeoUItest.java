4506e61ad34ec48223483690172754c472c760fe
RalphNom <53502331+RalphNom@users.noreply.github.com>
2020-10-20 23:36:44 -0700

    Adding FP

--- app/src/androidTest/java/com/bcit/comp7082/group1/GeoUItest.java
+++ app/src/androidTest/java/com/bcit/comp7082/group1/GeoUItest.java
@@ -1,10 +1,13 @@
 package com.bcit.comp7082.group1;
 
+import android.location.Location;
+
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
 
 import com.bcit.com7082.group1.activity.MainActivity;
+import com.bcit.com7082.group1.presenter.Helper;
 
 import org.junit.Rule;
 import org.junit.Test;
@@ -28,11 +31,16 @@ import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
 public class GeoUItest{
 
-    //Help.geoTag(Fakefile, 50, -100)
+        public void geoTagImage(File photoFile, Location location) {
+        Helper.geoTag(photoFile.getPath(), location.getLatitude(), location.getLongitude());
+    }
 
-    private File createImageFile(String caption) throws IOException {
+    private File createImageFile(String caption, File photoFile, Location location) throws IOException {
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
         String imageFileName = "JPEG_" + timeStamp + "_" + caption + "_";
+
+        Helper.geoTag(photoFile.getPath(), location.getLatitude(), location.getLongitude());
+
         File storageDir = new File("/storage/emulated/0/Android/data/com.bcit.comp7082.group1/files/Pictures/");
         File image = File.createTempFile(imageFileName, ".jpg", storageDir);
         return image;
@@ -45,15 +53,16 @@ public class GeoUItest{
     public void GeoTest() throws IOException
     {
 
-        //Searches for GPS, hard coded for now no way to get Location from mainactivity, adjust to what your location is
+        File file = createImageFile("TestCaption", "/storage/emulated/0/Android/data/com.bcit.comp7082.group1/files/Pictures/", 200.0, 50.0);
+
         onView(withId(R.id.search_button)).perform(click());
-        onView(withId(R.id.etLatitudeFrom)).perform(typeText("37"), closeSoftKeyboard());
-        onView(withId(R.id.etLatitudeTo)).perform(typeText("90"), closeSoftKeyboard());
-        onView(withId(R.id.etLongitudeFrom)).perform(typeText("-150"), closeSoftKeyboard());
-        onView(withId(R.id.etLongitudeTo)).perform(typeText("-100"), closeSoftKeyboard());
+        onView(withId(R.id.etLatitudeFrom)).perform(typeText("100.0"), closeSoftKeyboard());
+        onView(withId(R.id.etLatitudeTo)).perform(typeText("300.0"), closeSoftKeyboard());
+        onView(withId(R.id.etLongitudeFrom)).perform(typeText("50.0"), closeSoftKeyboard());
+        onView(withId(R.id.etLongitudeTo)).perform(typeText("150.0"), closeSoftKeyboard());
         onView(withId(R.id.go)).perform(click());
+        onView(withId(R.id.RightButton)).perform(click());
+        onView(withId(R.id.LeftButton)).perform(click());
 
     }
-
-
 }
